a3e85451094096cebd108f369baeb41d172526cd
khakomaki <hakomaki.karri@gmail.com>
2025-06-20 19:46:42 +0300

    Linter cleanup
    Got rid of unused imports.
    Fixed typos.

--- androidApp/src/main/java/com/khakomaki/memecontainer/ui/screens/home/HomeScreen.kt
+++ androidApp/src/main/java/com/khakomaki/memecontainer/ui/screens/home/HomeScreen.kt
@@ -3,16 +3,8 @@ package com.khakomaki.memecontainer.ui.screens.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
